9269abcda0cb740da59bb12171232e0a3d3c14f2
许睿睿 <xrr22223@163.com>
2019-01-25 16:21:43 +0800

    初次init myvue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- 放置顶部header区域 -->
+    <mt-header fixed title="黑马程序员vue项目"></mt-header>
+    <!-- 中间路由router-view区域 -->
+    <router-view/>
+    <!-- 底部tabbar区域 -->
+   <nav class='mui-bar mui-bar-tab'>
+     <a class='mui-tab-item mui-active' href="#tabbar">
+       <span class='mui-icon mui-icon-home'></span>
+       <span class='mui-tab-label'>首页</span>
+     </a>
+      <a class='mui-tab-item ' href="#tabbar">
+       <span class='mui-icon mui-icon-email'></span>
+       <span class='mui-tab-label'>首页</span>
+     </a>
+      <a class='mui-tab-item ' href="#tabbar">
+       <span class='mui-icon mui-icon-contact'></span>
+       <span class='mui-tab-label'>首页</span>
+     </a>
+      <a class='mui-tab-item ' href="#tabbar">
+       <span class='mui-icon mui-icon-gear'></span>
+       <span class='mui-tab-label'>首页</span>
+     </a>
+   </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  padding-top:40px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+//导入app组件
+import App from './App'
+//导入自定义路由模块
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+//导入Mint-ui
+import MintUI from 'mint-ui'
+//这里可以省略node_moudules这一层目录
+import 'mint-ui/lib/style.css'
+//将MintUI安装到vue中
+Vue.use(MintUI)
+
+//导入MUI的样式
+import './lib/mui/css/mui.min.css'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
